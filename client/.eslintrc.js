4e511c28749adbdb17f2beeb7a45ab7d5f61c854
Johnny Kan <johnnykan@Johnnys-MacBook-Pro.local>
2020-09-22 12:49:47 +0200

    Push client folder

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    // 'eslint:recommended',
+    'plugin:react/recommended',               // eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',  // @typescript-eslint/eslint-plugin
+  ],
+  plugins: [
+    'react'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,                        // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',                     // Allows for the use of imports
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,                              // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'no-use-before-define': 'off',                        // Disable eslint base rule.
+    '@typescript-eslint/no-use-before-define': ['error']  // Enable TS rule.
+  },
+  settings: {
+    react: {
+      version: 'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
