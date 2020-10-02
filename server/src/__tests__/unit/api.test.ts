eea19b63c35d700c9c912c1651ef78c393fb94ab
Johnny Kan <kan.johnny@gmail.com>
2020-10-02 19:29:03 +0200

    Add unit tests

---
+++ server/src/__tests__/unit/api.test.ts
@@ -0,0 +1,74 @@
+import request from 'supertest';
+import express from 'express';
+import bodyParser from 'body-parser';
+import validation from '../../api/validation';
+
+jest.mock('../../utils/users');
+import { usernames } from '../../utils/users';
+(usernames as jest.Mock).mockReturnValue([]); // Set dummy mock to default value.
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/validation', validation);
+
+describe('POST /api/validation route', () => {
+  it('Should respond 200 for valid username', (done: jest.DoneCallback) => {
+    request(app)
+      .post('/api/validation')
+      .send({ name: 'validName' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect({
+        valid: true,
+        msg: ''
+      })
+      .end(done)
+  });
+
+  it('Should respond 400 for empty username', (done: jest.DoneCallback) => {
+    request(app)
+      .post('/api/validation')
+      .send({ name: '' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect({
+        valid: false,
+        msg: 'Empty string is not allowed. Please enter a valid name.'
+      })
+      .end(done)
+  });
+
+  it('Should respond 400 on username with special character(s)', (done: jest.DoneCallback) => {
+    request(app)
+      .post('/api/validation')
+      .send({ name: 'NameWith$pecialCharacter' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect({
+        valid: false,
+        msg: 'Special characters are not allowed. Please enter a valid name.'
+      })
+      .end(done)
+  });
+
+  it('Should respond 400 on username that already exist', (done: jest.DoneCallback) => {
+    const alreadyExistingName: string = 'Barack_Obama';
+    
+    (usernames as jest.Mock).mockReturnValue([alreadyExistingName]); // Real mock!
+
+    request(app)
+      .post('/api/validation')
+      .send({ name: alreadyExistingName })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect({
+        valid: false,
+        msg: 'The name is already taken. Try another one.'
+      })
+      .end(done)
+  });
+});
---
+++ server/src/__tests__/unit/functions.test.ts
@@ -0,0 +1,66 @@
+import { assert } from 'chai';
+import messageObj from '../../utils/messages';
+import { TMessage } from '../../utils/types';
+import { hasSpecialChar, nameExist } from '../../utils/validation';
+
+jest.mock('../../utils/users');
+import { usernames } from '../../utils/users';
+const { storeUser, removeUser } = jest.requireActual('../../utils/users');
+
+const mockName: string = 'John';
+const mockMsg: string = 'This is a message.';
+
+describe('Username validation functions', () => {
+  it('Should check for special characters', () => {
+    const withSpecialChar = hasSpecialChar('John_1$');
+    const withoutSpecialChar = hasSpecialChar('John_1');
+    
+    assert.equal(withSpecialChar, true);
+    assert.equal(withoutSpecialChar, false);
+  });
+
+  it('Should check if name already exists', () => {
+    const mockUsersStore: string[] = ['John', 'Jane'];
+    const newUnvalidUsername: string = 'John';
+    const newValidUsername: string = 'John2';
+
+    (usernames as jest.Mock).mockReturnValue(mockUsersStore);
+
+    assert.equal(nameExist(newUnvalidUsername), true);
+    assert.equal(nameExist(newValidUsername), false);
+    assert.equal((usernames as jest.Mock).mock.calls.length, 2); // Mock func called twice.
+  });
+});
+
+describe('Message helper function', () => {
+  it('Should return an object with correct data', () => {
+    const response: TMessage = messageObj(mockName, mockMsg);
+    const currentTime: string = (new Date()).toLocaleTimeString().slice(0, 5);
+
+    assert.isObject(response);
+    assert.hasAllKeys(response, ['author', 'time', 'message']);
+    assert.equal(response.author, mockName);
+    assert.equal(response.time, currentTime);
+    assert.equal(response.message, mockMsg);
+  });
+});
+
+describe('Users helper functions', () => {
+  it('Should return correct array of usernames after storing data', () => {
+    let users: string[] = storeUser({ id: '1', username: 'John' });
+    
+    assert.isArray(users);
+    assert.sameMembers(users, ['John']);
+    
+    users = storeUser({ id: '2', username: 'Sara' });
+    assert.lengthOf(users, 2);
+    assert.sameMembers(users, ['John', 'Sara']);
+  });
+
+  it('Should return correct array of usernames after removal of data', () => {
+    let users: string[] = removeUser('1');
+    
+    assert.lengthOf(users, 1);
+    assert.sameMembers(users, ['Sara']);
+  });
+});
