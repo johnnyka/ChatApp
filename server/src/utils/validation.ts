b49ed571ce61553c3cfd4b53a65c78904d22a9d0
Johnny Kan <kan.johnny@gmail.com>
2020-10-02 17:00:13 +0200

    Correct regex

--- server/src/utils/validation.ts
+++ server/src/utils/validation.ts
@@ -4,5 +4,5 @@ export const hasSpecialChar = (username: string): boolean => /\W/.test(username)
 
 export const nameExist = (username: string): boolean => {
   const allUsers = usernames();
-  return allUsers.some((name) => new RegExp(username, 'i').test(name));
+  return allUsers.some((name) => new RegExp(`^${username}$`, 'i').test(name));
 };
